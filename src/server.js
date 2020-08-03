761da0ff7a507d3f2c9a824435d32f676c49ea91
Alexandre <lelex.g@hotmail.com>
2020-08-03 04:10:28 -0300

    trying to run on vercel

--- src/server.js
+++ src/server.js
@@ -5,28 +5,30 @@ const { createContext } = require('./context');
 const express = require('express')
 const cors = require('cors');
 
-const server = new ApolloServer({ 
-  schema, 
-  context: createContext,
-  introspection: true,
-  playground: true,
-});
-
-const app = express();
-
-var corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true // includes headers for the requests to backend
-}
-
-app.use(cors(corsOptions));
-
-server.applyMiddleware({
-  app,
-  path: '/',
-  cors: false // disbles default apollo-server cors and uses the express middleware cors in-lieu. 
+const server = new ApolloServer({
+    schema,
+    context: createContext,
+    playground: true,
+    introspection: true,
 })
 
-app.listen( 4000, () => {
-  console.log(`🚀 Server ready at http://localhost:4000`)
-});
+// const app = express();
+
+// var corsOptions = {
+//   origin: 'http://localhost:3000',
+//   credentials: true // includes headers for the requests to backend
+// }
+
+// app.use(cors(corsOptions));
+
+// server.applyMiddleware({
+//   app,
+//   path: '/',
+//   cors: false // disbles default apollo-server cors and uses the express middleware cors in-lieu. 
+// })
+
+// app.listen( 4000, () => {
+//   console.log(`🚀 Server ready at http://localhost:4000`)
+// });
+  
+export default server.createHandler({ path: '/graphql' })
