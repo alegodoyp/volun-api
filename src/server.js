c91af656d2670e8221c9572e1b8096230f9e530a
Alexandre <lelex.g@hotmail.com>
2021-04-29 21:46:22 -0300

    serverless try

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-micro');
 const { schema } = require('./schema');
 const { createContext } = require('./context');
-const express = require('express')
-const cors = require('cors');
+// const express = require('express')
+// const cors = require('cors');
 
 const server = new ApolloServer({
     schema,
@@ -11,23 +11,31 @@ const server = new ApolloServer({
     introspection: true,
 })
 
-const app = express();
+//const app = express();
 
-var corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true // includes headers for the requests to backend
-}
+// var corsOptions = {
+//   origin: 'http://localhost:3000',
+//   credentials: true // includes headers for the requests to backend
+// }
 
-app.use(cors(corsOptions));
+// app.use(cors());
 
-server.applyMiddleware({
-  app,
-  path: '/',
-  cors: false // disbles default apollo-server cors and uses the express middleware cors in-lieu. 
-})
+// server.applyMiddleware({
+//   app,
+//   path: '/',
+//   cors: false // disbles default apollo-server cors and uses the express middleware cors in-lieu. 
+// })
 
-app.listen( 4000, () => {
-  console.log(`🚀 Server ready at http://localhost:4000`)
+// app.listen( 4000, () => {
+//   console.log(`🚀 Server ready at http://localhost:4000`)
+// });
+
+module.exports = server.createHandler({
+  path: '/api/graphql',
 });
 
-  
+module.exports.config = {
+  api: {
+    bodyParser: false,
+  },
+};
